3eeea5a8c285a26fa839385af75b7708e30ce59d
Limitlex Technologies <kobbygazi@gmail.com>
2025-12-13 12:16:32 +0000

    Updated files

--- task_0/webpack.config.js
+++ task_0/webpack.config.js
@@ -15,7 +15,7 @@ class LogNoTypeErrorsPlugin {
 }
 
 module.exports = {
-  entry: path.resolve(__dirname, 'main.ts'),
+  entry: path.resolve(__dirname, 'js/main.ts'),
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, '../dist'),
