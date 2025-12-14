84cde4df1007663351d991341a74dce0a691fc0b
Limitlex Technologies <kobbygazi@gmail.com>
2025-12-14 15:34:08 +0000

    Fixed error

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -1,16 +1,16 @@
-interface DirectorInterface {
+export interface DirectorInterface {
     workFromHome(): string;
     getCoffeeBreak(): string;
     workDirectorTasks(): string;
 }
 
-interface TeacherInterface {
+export interface TeacherInterface {
     workFromHome(): string;
     getCoffeeBreak(): string;
     workTeacherTasks(): string;
 }
 
-class Director implements DirectorInterface {
+export class Director implements DirectorInterface {
     workFromHome(): string {
         return 'Working from home';
     }
@@ -22,7 +22,7 @@ class Director implements DirectorInterface {
     }
 }
 
-class Teacher implements TeacherInterface {
+export class Teacher implements TeacherInterface {
     workFromHome(): string {
         return 'Cannot work from home';
     }
@@ -34,24 +34,23 @@ class Teacher implements TeacherInterface {
     }
 }
 
-function createEmployee(salary: number | string): Director | Teacher {
+export function createEmployee(salary: number | string): Director | Teacher {
     if (typeof salary === 'number' && salary < 500) {
         return new Teacher();
     }
     return new Director();
 }
 
-function isDirector(employee: Teacher | Director): employee is Director {
+export function isDirector(employee: Teacher | Director): employee is Director {
     return (employee as Director).workDirectorTasks !== undefined;
 }
 
-function executeWork(employee: Teacher | Director): void {
+export function executeWork(employee: Teacher | Director): string {
     if (isDirector(employee)) {
-        console.log(employee.workDirectorTasks());
-    } else {
-        console.log(employee.workTeacherTasks());
+        return employee.workDirectorTasks();
     }
+    return employee.workTeacherTasks();
 }
 
-executeWork(createEmployee(200));
-executeWork(createEmployee(1000));
+console.log(executeWork(createEmployee(200)));
+console.log(executeWork(createEmployee(1000)));
